db0d448455cbe355f3eed72b93f59405281b6c14
user <user@debian-10.localdomain>
2020-04-06 09:49:25 -0400

    Added write operation

--- chardev.c
+++ chardev.c
@@ -3,6 +3,7 @@
 #include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/module.h>
+#include <linux/uaccess.h>
 
 
 /*  
@@ -21,6 +22,7 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 #define SUCCESS 0
 #define FAIL -1
 #define DEVICE_NAME "chardev"
+#define WRITE_BUF_SIZE 256
 
 /* 
  * Global variables are declared as static, so are global within the file. 
@@ -99,8 +101,7 @@ static void myexit(void)
  */
 static int device_open(struct inode *inode, struct file *file)
 {
-	printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
-	return -EINVAL;
+	return 0;
 }
 
 /* 
@@ -108,8 +109,7 @@ static int device_open(struct inode *inode, struct file *file)
  */
 static int device_release(struct inode *inode, struct file *file)
 {
-	printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
-	return -EINVAL;
+	return 0;
 }
 
 /* 
@@ -121,7 +121,7 @@ static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
 			   size_t length,	/* length of the buffer     */
 			   loff_t * offset)
 {
-	printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
+	printk(KERN_ALERT "Read not implemented yet.\n");
 	return -EINVAL;
 }
 
@@ -129,10 +129,20 @@ static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
  * Called when a process writes to dev file: echo "hi" > /dev/hello 
  */
 static ssize_t
-device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
+device_write(struct file *filp, const char *input_buff, size_t input_len, loff_t * off)
 {
-	printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
-	return -EINVAL;
+	char buf[WRITE_BUF_SIZE];
+	if (input_len > 255) {
+		printk(KERN_ALERT "Write with input_len too big.\n");
+		return -EINVAL;
+	}
+	if (copy_from_user(buf, input_buff, input_len) > 0) {
+		printk(KERN_ALERT "Could not copy data from userspace.\n");
+		return -EIO;
+	}
+	buf[input_len] = '\0';
+	printk(KERN_ALERT "%s\n", buf);
+	return input_len;
 }
 
 module_init(myinit)
